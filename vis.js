3e742526a08d60aa54c72878f4a6624bfac3063a
lijun zhou <129286838+Q-g2@users.noreply.github.com>
2023-11-10 08:14:40 +1100

    Add files via upload
    all files A2

---
+++ vis.js
@@ -0,0 +1,34 @@
+var spec1 = "js/globalmap.vg.json";
+vegaEmbed("#globalmap", spec1, {"actions": false})
+  .then(function (result) {})
+  .catch(console.error);
+
+var spec2 = "js/boxplot.vg.json";
+vegaEmbed("#boxplot", spec2, {"actions": false})
+  .then(function (result) {})
+  .catch(console.error);
+
+var spec3 = "js/diverge_stackbar.vg.json";
+vegaEmbed("#diverge-stackbar", spec3, {"actions": false})
+  .then(function (result) {})
+  .catch(console.error);
+
+var spec4 = "js/heatmap.vg.json";
+vegaEmbed("#heatmap", spec4, {"actions": false})
+  .then(function (result) {})
+  .catch(console.error);
+
+var spec5 = "js/lolipop.vg.json";
+vegaEmbed("#lolipop", spec5, {"actions": false})
+  .then(function (result) {})
+  .catch(console.error);
+
+var spec6 = "js/radialchart.vg.json";
+vegaEmbed("#radial-chart", spec6, {"actions": false})
+  .then(function (result) {})
+  .catch(console.error);
+
+var spec6 = "js/pm2_5_2D_scatterplot.vg.json";
+vegaEmbed("#2D-scatterplot", spec6, {"actions": false})
+  .then(function (result) {})
+  .catch(console.error);
