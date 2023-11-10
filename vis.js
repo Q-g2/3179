4036bf8e0a54414120fba56335dd386e3d2ef011
lijun zhou <129286838+Q-g2@users.noreply.github.com>
2023-11-10 17:52:44 +1100

    Update vis.js
    change directory because nil folder

--- vis.js
+++ vis.js
@@ -1,34 +1,34 @@
-var spec1 = "js/globalmap.vg.json";
+var spec1 = "globalmap.vg.json";
 vegaEmbed("#globalmap", spec1, {"actions": false})
   .then(function (result) {})
   .catch(console.error);
 
-var spec2 = "js/boxplot.vg.json";
+var spec2 = "boxplot.vg.json";
 vegaEmbed("#boxplot", spec2, {"actions": false})
   .then(function (result) {})
   .catch(console.error);
 
-var spec3 = "js/diverge_stackbar.vg.json";
+var spec3 = "diverge_stackbar.vg.json";
 vegaEmbed("#diverge-stackbar", spec3, {"actions": false})
   .then(function (result) {})
   .catch(console.error);
 
-var spec4 = "js/heatmap.vg.json";
+var spec4 = "heatmap.vg.json";
 vegaEmbed("#heatmap", spec4, {"actions": false})
   .then(function (result) {})
   .catch(console.error);
 
-var spec5 = "js/lolipop.vg.json";
+var spec5 = "lolipop.vg.json";
 vegaEmbed("#lolipop", spec5, {"actions": false})
   .then(function (result) {})
   .catch(console.error);
 
-var spec6 = "js/radialchart.vg.json";
+var spec6 = "radialchart.vg.json";
 vegaEmbed("#radial-chart", spec6, {"actions": false})
   .then(function (result) {})
   .catch(console.error);
 
-var spec6 = "js/pm2_5_2D_scatterplot.vg.json";
+var spec6 = "pm2_5_2D_scatterplot.vg.json";
 vegaEmbed("#2D-scatterplot", spec6, {"actions": false})
   .then(function (result) {})
   .catch(console.error);
